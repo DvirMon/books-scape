5ed57ea3cfd87fd1282761fd45dafa4ba247c765
DvirMon <dmenajem@gmail.com>
2023-08-06 14:55:07 +0300

    feat (store.service.ts) - store.service works with signals

--- src/app/books/books.service.ts
+++ src/app/books/books.service.ts
@@ -1,5 +1,5 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { Injectable, WritableSignal, signal } from '@angular/core';
 import { Observable, map, tap } from 'rxjs';
 import { Book } from './books';
 
@@ -10,25 +10,27 @@ import { Book } from './books';
 export class BooksService {
 
   private readonly API_URL = 'https://www.googleapis.com/books/v1/volumes';
-  private readonly MAX_RESULTS : number = 10
+  private readonly MAX_RESULTS: number = 10
   private readonly BOOKS_API_KEY = 'AIzaSyAZSKuvnFiV9QTXM7NI6HgwpBp4m8rzSuM'
 
   constructor(
     private http: HttpClient
   ) { }
 
+
+
   // function to fetch books from Google Books API
   public getBooks(query?: string): Observable<Book[]> {
     return this.http.get<any>(`${this.API_URL}?q=intitle:${query}&langRestrict=en&maxResults=${this.MAX_RESULTS}&key=${this.BOOKS_API_KEY}`).pipe(
       map((res => res.items.filter((book: any) => book.volumeInfo.language === 'en'))),
-        map(items =>
-          items.map((book: any) => this.transformBookData(book.volumeInfo))
-          .filter((book: Book) => book.imageLinks != null)),
+      map(items =>
+        items.map((book: any) => this.transformBookData(book.volumeInfo))
+        .filter((book: Book) => book.imageLinks != null)),
 
-          )
-
-
+        )
   }
+
+
 
   // RxJS operator function to transform book data
   private transformBookData(volumeInfo: any): Book {
@@ -39,7 +41,7 @@ export class BooksService {
       description: volumeInfo.description,
       publishedDate: volumeInfo.publishedDate,
       imageLinks: volumeInfo.imageLinks,
-      categories : volumeInfo.categories
+      categories: volumeInfo.categories
     };
   }
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,9 +1,9 @@
-import { Component, inject } from '@angular/core';
+import { Component, Signal, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BooksService } from 'src/app/books/books.service';
 import { BookCardComponent } from 'src/app/books/book-card/book-card.component';
-import { Observable } from 'rxjs';
 import { Book } from 'src/app/books/books';
+import { StoreService } from 'src/app/store.service';
 
 @Component({
   selector: 'app-home',
@@ -14,8 +14,19 @@ import { Book } from 'src/app/books/books';
 })
 export class HomeComponent {
 
-  booksService : BooksService = inject(BooksService);
+  booksService: BooksService = inject(BooksService);
+  storeService: StoreService = inject(StoreService);
 
-  books$ : Observable<Book[]> = this.booksService.getBooks("harry potter")
+  public readonly books: Signal<Book[]>
+
+  constructor() {
+    this.books = this.storeService.selectBooks
+
+    this.booksService.getBooks().subscribe((books) => this.storeService.update({ books }))
+  }
+
+  termChanged() {
+
+  }
 
 }
