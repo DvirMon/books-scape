c295ca0a40882e701e4f54bc14a785bdd5fc1c6a
DvirMon <dmenajem@gmail.com>
2023-08-11 09:04:25 +0300

    feat (books.d.ts) - add more interface

--- src/app/books/books.d.ts
+++ src/app/books/books.d.ts
@@ -1,11 +1,66 @@
 export interface Book {
   id: string;
   title: string;
-  authors: string[];
+  // authors: string[];
   description: string;
-  publishedDate: string;
+  // publishedDate: string;
   imageLinks: ImageLinks,
-  categories: string[];
+  // categories: string[];
+}
+
+export interface Root {
+  kind: string
+  totalItems: number
+  items: Item[]
+}
+
+export interface Item {
+  kind: string
+  id: string
+  etag: string
+  selfLink: string
+  volumeInfo: VolumeInfo
+  saleInfo: SaleInfo
+  accessInfo: AccessInfo
+  searchInfo: SearchInfo
+}
+
+export interface VolumeInfo {
+  title: string
+  subtitle?: string
+  authors: string[]
+  publisher: string
+  publishedDate: string
+  description: string
+  industryIdentifiers: IndustryIdentifier[]
+  readingModes: ReadingModes
+  pageCount: number
+  printType: string
+  categories: string[]
+  maturityRating: string
+  allowAnonLogging: boolean
+  contentVersion: string
+  panelizationSummary: PanelizationSummary
+  imageLinks: ImageLinks
+  language: string
+  previewLink: string
+  infoLink: string
+  canonicalVolumeLink: string
+}
+
+export interface IndustryIdentifier {
+  type: string
+  identifier: string
+}
+
+export interface ReadingModes {
+  text: boolean
+  image: boolean
+}
+
+export interface PanelizationSummary {
+  containsEpubBubbles: boolean
+  containsImageBubbles: boolean
 }
 
 export interface ImageLinks {
@@ -13,4 +68,36 @@ export interface ImageLinks {
   thumbnail: string
 }
 
+export interface SaleInfo {
+  country: string
+  saleability: string
+  isEbook: boolean
+}
+
+export interface AccessInfo {
+  country: string
+  viewability: string
+  embeddable: boolean
+  publicDomain: boolean
+  textToSpeechPermission: string
+  epub: Epub
+  pdf: Pdf
+  webReaderLink: string
+  accessViewStatus: string
+  quoteSharingAllowed: boolean
+}
+
+export interface Epub {
+  isAvailable: boolean
+  acsTokenLink?: string
+}
+
+export interface Pdf {
+  isAvailable: boolean
+  acsTokenLink?: string
+}
+
+export interface SearchInfo {
+  textSnippet: string
+}
 
